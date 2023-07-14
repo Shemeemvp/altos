f188bb92cf2dbcadc621c656542f6ff15c3453a4
Muhamed Shemeem VP <shemeemmuhamed132@gmail.com>
2023-07-14 11:34:27 +0530

    VS Code extentions - installation, checking

--- pytonbasics/FileHandling/filehandling.py
+++ pytonbasics/FileHandling/filehandling.py
@@ -2,15 +2,15 @@
 # 2. Create a text file myfile.txt in python and ask the user to write separate 3 lines of text with 3 inputs statement form the user.
 # 3. Write a program to read the contents of both the files created in the above programs and merge the contents into merge.txt
 
-'''def program1():
+"""def program1():
     f = open('intro.txt','w')
     text = input("Enter the text..")
     f.write(text)
     f.close()
 
-program1()'''
+program1()"""
 
-'''def program2():
+"""def program2():
     f = open('myfile.txt','w')
     text1 = input('Enter first line..')
     text2 = input('Enter second line..')
@@ -22,18 +22,20 @@ program1()'''
     # f.writelines([text1,text2,text3])
     f.close()
 
-program2()'''
+program2()"""
+
 
 def program3():
     fileData1 = fileData2 = ""
-    f = open('intro.txt','r')
+    f = open("intro.txt", "r")
     fileData1 = f.read()
     f.close()
-    f = open('myfile.txt','r')
+    f = open("myfile.txt", "r")
     fileData2 = f.read()
     f.close()
 
-    f = open('merge.txt','w')
-    f.write(fileData1+'\n'+fileData2)
+    f = open("merge.txt", "w")
+    f.write(fileData1 + "\n" + fileData2)
+
 
 program3()
--- pytonbasics/sample.py
+++ pytonbasics/sample.py
@@ -1,94 +1,95 @@
 import mysql.connector
-mydb = mysql.connector.connect(host = 'localhost',user = 'root', database = 'mydatabase');
+
+mydb = mysql.connector.connect(host="localhost", user="root", database="mydatabase")
 mycursor = mydb.cursor()
 
 # mycursor.execute('create database mydatabase');
-'''mycursor.execute('show databases')
+"""mycursor.execute('show databases')
 
 for x in mycursor:
-    print(x)'''
+    print(x)"""
 
 # TABLE CREATION
 # mycursor.execute('create table customer(name varchar(50), address varchar(100))')
-'''mycursor.execute('show tables')
+"""mycursor.execute('show tables')
 for x in mycursor:
-    print(x)'''
+    print(x)"""
 
-# PRIMARY KEY 
+# PRIMARY KEY
 # mycursor.execute('create table person(id int auto_increment primary key, name varchar(50), age int)');
 
 # INSERTION
 
-'''sql = 'insert into person (name,age) values (%s,%s)'
+"""sql = 'insert into person (name,age) values (%s,%s)'
 value = ('Arjun',25)
 mycursor.execute(sql,value);
 mydb.commit()
-print(mycursor.rowcount,'Record inserted..')'''
+print(mycursor.rowcount,'Record inserted..')"""
 
 
 # MULTIPLE INSERTION
-'''sql = 'insert into person (name,age) values (%s,%s)';
+"""sql = 'insert into person (name,age) values (%s,%s)';
 value = [('Abhi',22),('Ashik',23),('Ajay',28),('karthi',40)]
 mycursor.executemany(sql,value)
 mydb.commit()
-print(mycursor.rowcount, 'records inserted..')'''
+print(mycursor.rowcount, 'records inserted..')"""
 
 
 # SELECTION
-'''mycursor.execute('select * from person');
+"""mycursor.execute('select * from person');
 myresult = mycursor.fetchall();
 for x in myresult:
-    print(x)'''
+    print(x)"""
 
-'''mycursor.execute('select * from person');
+"""mycursor.execute('select * from person');
 myresult = mycursor.fetchone();
-print(myresult)'''
+print(myresult)"""
 
 # SELECTION USING WHERE CONDITION
-'''sql = 'select * from person where name = "Arjun"'
+"""sql = 'select * from person where name = "Arjun"'
 mycursor.execute(sql)
 myresult = mycursor.fetchall()
 for x in myresult:
-    print(x)'''
+    print(x)"""
 
-'''sql = 'select * from person where name like "a_%"'
+"""sql = 'select * from person where name like "a_%"'
 mycursor.execute(sql)
 myresult = mycursor.fetchall()
 for x in myresult:
-    print(x)'''
+    print(x)"""
 
 # SORTING
-'''sql = 'select * from person order by name'
+"""sql = 'select * from person order by name'
 mycursor.execute(sql)
 myresult = mycursor.fetchall()
 for x in myresult:
-    print(x)'''
+    print(x)"""
 
-'''sql = 'select * from person order by name desc'
+"""sql = 'select * from person order by name desc'
 mycursor.execute(sql)
 myresult = mycursor.fetchall()
 for x in myresult:
-    print(x)'''
+    print(x)"""
 
 # DELETION
-'''sql = 'delete from person where id = 4'
+"""sql = 'delete from person where id = 4'
 mycursor.execute(sql)
 mydb.commit()
-print(mycursor.rowcount, 'deleted..')'''
+print(mycursor.rowcount, 'deleted..')"""
 
 # UPDATION
-'''sql = 'update person set name ="shemeem" where id = 2'
+"""sql = 'update person set name ="shemeem" where id = 2'
 mycursor.execute(sql)
 mydb.commit()
-print(mycursor.rowcount, 'row updated..')'''
+print(mycursor.rowcount, 'row updated..')"""
 
 # SELECTION USING LIMIT AND OFFSET
-'''mycursor.execute('select * from person limit 3')
+"""mycursor.execute('select * from person limit 3')
 myresult = mycursor.fetchall()
 for x in myresult:
-    print(x)'''
+    print(x)"""
 
-'''mycursor.execute('select * from person limit 3 offset 2')
+"""mycursor.execute('select * from person limit 3 offset 2')
 myresult = mycursor.fetchall()
 for x in myresult:
-    print(x)'''
+    print(x)"""
